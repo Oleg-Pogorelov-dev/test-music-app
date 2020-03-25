44c7ac1db93e454c3b5d5aed76a80a4da064dcfd
Oleg-Pogorelov-levi <debagull@gmail.com>
2020-03-25 12:47:31 +0300

    Attached app

---
+++ src/MainPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function MainPage() {
+  return (
+    <iframe src="http://localhost:3000" frameBorder="0" style={{ width: '100%', height: '100vh' }}></iframe>
+  );
+}
+
